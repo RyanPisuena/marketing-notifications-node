999ea71692bf1b8694ddc5b4b45fa3ec55cc1186
Ryan Pisuena <Pisuenaryan@gmail.com>
2018-03-03 13:31:03 -0800

    Add new function

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ mongoose.Promise = Promise;
 // Create Express web app
 const app = require('./webapp');
 
+app.get('/', function(req, res){
+  res.send('hello world');
+}); 
+
+
 // Create an HTTP server and listen on the configured port
 const server = http.createServer(app);
 server.listen(config.port, function() {
